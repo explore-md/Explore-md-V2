38336d1ce5f4b1ebb45871b878139c07d8b243a7
explore-md <cyberreddragon377@gmail.com>
2025-09-28 12:23:27 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝐄𝐗𝐏𝐋𝐎𝐑𝐄 𝐌𝐃',
+  author: '‎',
+  botName: "𝐄𝐗𝐏𝐋𝐎𝐑𝐄 𝐌𝐃 𝐕2",
+  botOwner: 'REDDRAGON DFS', // Your name
+  ownerNumber: '27634988678', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.2.0",
+  updateZipUrl: "https://github.com/explore-md/Explore-md-V2/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
